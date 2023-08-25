dcb026fabdf8a421dd8b29ec95afe32c9549f278
andreheller87 <andreheller02@gmail.com>
2023-08-25 11:47:06 -0300

    mes

--- ProjetoAula09/Switch.java
+++ ProjetoAula09/Switch.java
@@ -6,7 +6,34 @@ public class Switch
         Scanner in = new Scanner(System.in);
         int teste;
         
+        System.out.println("Digite o mês em número inteiro");
+        teste = in.nextInt();
+        
+        switch(teste){
+                case 1:
+                 System.out.println("Janeiro");
+                 break;
+                 case 2:
+                 System.out.println("Fevereiro");
+                 break;
+
+                 case 3:
+                 System.out.println("Março");
+                 break;
+                 
+                 case 4:
+                 System.out.println("Abril");
+                 break;
+                                  
+                 default:
+                      System.out.println("Digite SOMENTE números entre 1 e 4");
+                      break;
+                 
+                 
+
+        }
         
     }
    
+}
 }
