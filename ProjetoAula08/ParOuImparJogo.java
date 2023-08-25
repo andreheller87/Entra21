ba1aa2b92704658d42613c8b05d5a96de23350b5
andreheller87 <andreheller02@gmail.com>
2023-08-25 09:32:16 -0300

    update

--- ProjetoAula08/ParOuImparJogo.java
+++ ProjetoAula08/ParOuImparJogo.java
@@ -5,33 +5,34 @@ public class ParOuImparJogo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
+        
         System.out.println("++++++++++++++++++++");
         System.out.println("Jogo Par ou Ímpar!");
-         System.out.println("++++++++++++++++++++");
-          System.out.println();
+        System.out.println("++++++++++++++++++++");
+        System.out.println();
         System.out.print("Digite seu nome: ");
         String nomeUsuario = scanner.nextLine();
-        
+
         int vitoriasUsuario = 0;
         int vitoriasComputador = 0;
-        
+
         while (true) {
             int numeroUsuario;
-            
+
             do {
                 System.out.print("\nDigite um número entre 0 e 5: ");
                 numeroUsuario = scanner.nextInt();
             } while (numeroUsuario < 0 || numeroUsuario > 5);
-            
+
             int numeroComputador = random.nextInt(6); 
-            
+
             System.out.println(nomeUsuario + " escolheu " + numeroUsuario);
             System.out.println("Computador escolheu " + numeroComputador);
-            
+
             int soma = numeroUsuario + numeroComputador;
-            
+
             System.out.println("A soma dos números é: " + soma);
-            
+
             if (soma % 2 == 0) {
                 System.out.println("A soma é PAR!");
                 if (soma == 0) {
@@ -52,13 +53,13 @@ public class ParOuImparJogo {
             System.out.println();
             System.out.print("Deseja jogar novamente? (S/N): ");
             String jogarNovamente = scanner.next();
-            
+
             if (!jogarNovamente.equalsIgnoreCase("S")) {
                 System.out.println("Obrigado por jogar!");
                 break;
             }
         }
-        
+
         scanner.close();
     }
 }
