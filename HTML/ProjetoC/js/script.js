95bcdc1de4809cb537cb7a32f056986801a1159f
andreheller87 <andreheller02@gmail.com>
2023-10-17 08:51:58 -0300

    update

---
+++ HTML/ProjetoC/js/script.js
@@ -0,0 +1,13 @@
+function doDemo(button){
+var square = document.getElementById("square");
+square.style.backgroundColor = "#fa4";
+button.setAttribute("disabled","true");
+seTimeout(clearDemo, 2000, button);
+}
+
+function clearDemo(button){
+    var square = document.getElementById("square");
+    square.style.backgroundColor = "transparent";
+    button.removeAttribute("disable");
+    
+}
