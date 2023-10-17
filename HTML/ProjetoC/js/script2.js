0b65fe00f584b508c858fe945d8f8c2ed6f9e26e
andreheller87 <andreheller02@gmail.com>
2023-10-17 09:26:39 -0300

    update

---
+++ HTML/ProjetoC/js/script2.js
@@ -0,0 +1,8 @@
+var quantos = prompt("quantos x's você quer ver?(de 1 a 10 está de bom tamanho)" , "");
+var xises = "";
+var loop = 0;
+while(loop<quantos){
+    xises = xises + "x";
+    loop = loop + 1;
+}
+alert(xises)
