a102e1a0bd4c3171411335249cdfd499b8ea61f6
andreheller87 <andreheller02@gmail.com>
2023-12-07 11:15:25 -0300

    update

--- Albuns/StockKeeper.java
+++ Albuns/StockKeeper.java
@@ -1,26 +1,31 @@
- 
 
 public class StockKeeper {
-
-	public String name;
-
-	public StockKeeper(String name) {
-		super();
-		this.name = name;
-	}
+    private String name;
 	
-	
-	public void manageAlbum(Album album,String name, String artist, double price,int numberOfCopies) {
-		album.setName(name);
-		album.setArtist(artist);
-		album.setPrice(price);
-		album.setNumberOfCopies(numberOfCopies);
-		
-		System.out.println("Album gerenciado por: " + this.name);
-		System.out.println("Detalhas do album:::::::::");
-		System.out.println("Nome do album: " + album.getName());
-		System.out.println("Artista do album: " + album.getArtist());
-		System.out.println("preço do album: " + album.getPrice());
-		System.out.println("Número de cópias do album: " + album.getNumberOfCopies());
-	}
+    public StockKeeper(String name) {
+       
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }	
+
+    public void manageAlbum(Album album,String name, String artist, double price,int numberOfCopies) {
+        album.setName(name);
+        album.setArtist(artist);
+        album.setPrice(price);
+        album.setNumberOfCopies(numberOfCopies);
+
+        System.out.println("Album gerenciado por: " + getName());
+        System.out.println("Detalhas do album:::::::::");
+        System.out.println("Nome do album: " + album.getName());
+        System.out.println("Artista do album: " + album.getArtist());
+        System.out.println("preço do album: " + album.getPrice());
+        System.out.println("Número de cópias do album: " + album.getNumberOfCopies());
+    }
 }
